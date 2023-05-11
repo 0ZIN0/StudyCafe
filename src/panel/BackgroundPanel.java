3625766febf1e79b2c713886c47cfe04b77a5e60
Gi7A-09 <Gi7A-09@DESKTOP-OI19DI3>
2023-05-11 11:36:49 +0900

    backgroundPanel 클래스 첫글자 B로 변경 및 생성자 매개변수에 컬러 추가해서 오버로딩

--- src/panel/BackgroundPanel.java
+++ src/panel/BackgroundPanel.java
@@ -7,15 +7,22 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
-public class backgroundPanel extends JPanel{
+public class BackgroundPanel extends JPanel{
 	
 	Image image;
 	
-	public backgroundPanel(ImageIcon icon) {
+	public BackgroundPanel(ImageIcon icon) {
 		this.image = icon.getImage();
 		setSize(image.getWidth(null),image.getHeight(null));
 		setBackground(new Color(0, 0, 0, 0));
 		setVisible(true);
+	}
+	
+	public BackgroundPanel(ImageIcon icon, Color color) {
+	    this.image = icon.getImage();
+	    setSize(image.getWidth(null),image.getHeight(null));
+	    setBackground(color);
+	    setVisible(true);
 	}
 	
 	protected void paintComponent(Graphics g) {
