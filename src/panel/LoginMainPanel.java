cbf92afe8b8dc2fc6130261706c057c90750e7e9
Gi7A-09 <Gi7A-09@DESKTOP-OI19DI3>
2023-05-16 13:56:10 +0900

    첫화면 추가중

---
+++ src/panel/LoginMainPanel.java
@@ -0,0 +1,154 @@
+package panel;
+
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.RootPaneContainer;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
+
+public class LoginMainPanel extends JPanel{
+	
+	CardLayout card = new CardLayout();
+
+	public LoginMainPanel() {
+		setLayout(null);
+		NumberKeypad numpad= new NumberKeypad();
+		//numpad.setBounds(0,0,500,500);
+		
+		
+		
+		BackgroundPanel background= new BackgroundPanel(new ImageIcon("ui/background/background.png"));
+		background.setLayout(null);
+		//background.setBounds(0,0,1920,1080);
+		add(background);
+		background.add(numpad);
+		numpad.setBounds(900,450,600,500);
+		
+		JPanel cardPanel = new JPanel();
+		cardPanel.setLayout(card);
+		
+		
+		
+		
+		
+		setBounds(0,0,1920,1080);
+		setVisible(true);
+		//setBorder(new TitledBorder(new LineBorder(Color.CYAN,1)));
+		
+		
+		
+	
+		
+		
+		//add(numpadPanel);
+		
+		
+		//LoginPanel m1= new LoginPanel();
+		
+		//MemberJoinPanel m2 =new MemberJoinPanel();
+		
+//		m1.add(m1.memberJoinBtn);
+		
+		//add(m1, "p1");
+		//add(m2, "p2");
+		
+		// 팝업 띄우기
+		//SeatPopup popup = new SeatPopup();
+		
+		//card.show(MainPanel.this, "p1");
+		
+//		m1.memberJoinBtn.addActionListener(new ActionListener() {
+//		    public void actionPerformed(ActionEvent e) {
+//		        card.show(LoginMainPanel.this, "p2");
+//		    }
+//		});
+		
+		
+		
+
+		
+		
+		
+		
+	}
+
+	
+
+//	protected static void SeatPopup() {
+//		JFrame frame = new JFrame("좌석 정보!!!!!!!");
+//		
+//		
+//		JPanel p = new JPanel();
+//		p.add(new JLabel("팝업 내용이야"));
+//		
+//		
+//		JDialog dialog = new JDialog(frame,"좌석 정보!!!",true);
+//		dialog.setAlwaysOnTop(true);
+//		dialog.add(p);
+//		dialog.setSize(500,500);
+//		dialog.setLocationRelativeTo(null);
+//		
+//		dialog.setVisible(true);
+//		
+//	}
+	
+}
+
+class NumberKeypad extends JPanel implements ActionListener {
+    JTextField textField;// 입력값을 보여줄 텍스트 필드
+    public  NumberKeypad() {
+        setLayout(new GridLayout(4, 3));  // 4행 3열의 그리드 레이아웃
+        
+        // 버튼 생성
+        for (int i = 1; i <= 9; i++) {
+            JButton button = new JButton(Integer.toString(i));
+            button.addActionListener(this);
+            add(button);
+        }
+         
+        // Clear 버튼 추가
+        JButton clearButton = new JButton("지우기");
+        clearButton.addActionListener(this);
+        add(clearButton);
+        
+        // 0 버튼 추가
+        JButton zeroButton = new JButton("0");
+        zeroButton.addActionListener(this);
+        add(zeroButton);
+        
+        JButton confirmButton = new JButton("확인");
+        clearButton.addActionListener(this);
+        add(confirmButton);
+    }
+    
+    // 버튼 클릭 시 호출되는 메서드
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        if (command.equals("지우기")) {  // Clear 버튼 클릭 시 텍스트필드 초기화
+        	textField.setText("");
+        } else {  // 숫자 버튼 클릭 시 해당 숫자를 텍스트 필드에 추가
+            textField.setText(textField.getText() + command);
+        }
+    }
+
+    public void setTextField(JTextField textField) {
+    	this.textField = textField;
+    }
+ }
+
+
---
+++ src/panel/LoginPanel.java
@@ -0,0 +1,133 @@
+package panel;
+
+import java.awt.Color;
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+
+public class LoginPanel extends JPanel {
+	
+	//public JButton memberJoinBtn;
+	
+	private static String driverName = "oracle.jdbc.driver.OracleDriver";
+	private static String url = "jdbc:oracle:thin:@localhost:1521:XE";
+	private static String user  = "teamproject";
+	private static String password = "1234";
+	
+	
+	
+	GridLayout grid = new GridLayout(5,5);
+	JTextField userPhonNumber = new JTextField();
+	JPasswordField userPassField = new JPasswordField();
+	JButton loginButton = new JButton("로그인");
+	JButton memberJoinBtn=new JButton("회원가입");
+	NumberKeypad numpad = new NumberKeypad();
+	
+	
+	public LoginPanel() {
+		
+		setBorder(new TitledBorder(new LineBorder(Color.red,5)));
+		setLayout(null);
+		setBounds(0,200,1920,880);
+		
+		//memberJoinBtn.setBounds(0,0,200,200);
+		
+		JPanel loginP = new JPanel(grid);
+		loginP.setBounds(0,0,500,500);
+		
+		NumberKeypad numpad = new NumberKeypad();
+		numpad.setBounds(0,0,300,200);
+		
+		loginP.setLayout(grid);
+		
+		add(loginP);
+		loginP.add(userPhonNumber);
+		loginP.add(userPassField);
+		loginP.add(loginButton);
+		loginP.add(memberJoinBtn);
+		add(numpad);
+		
+		
+		numpad.setBounds(1000,200,300,500);
+		
+		
+		loginP.setBounds(0,0,600,600);
+		
+		
+		
+		
+		
+		
+//		userPhonNumber.addActionListener(new ActionListener() {
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				String command = e.getActionCommand();
+//		        if (command.equals("지우기")) {  // Clear 버튼 클릭 시 텍스트필드 초기화
+//		        	textField.setText("");
+//		        } else {  // 숫자 버튼 클릭 시 해당 숫자를 텍스트 필드에 추가
+//		            textField.setText(textField.getText() + command);
+//		        }
+//				
+//			}
+//		});
+		
+		
+		
+		
+		
+		loginButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String phon = userPhonNumber.getText();
+				String pass = new String(userPassField.getPassword());
+				
+				try {
+					Connection conn = DriverManager.getConnection(url, user, password);
+					PreparedStatement stmt = conn.prepareStatement("SELECT * FROM member WHERE phone_number=? AND member_password=?");
+		            stmt.setString(1, phon);
+		            stmt.setString(2, pass);
+		            ResultSet rs = stmt.executeQuery();	
+		            
+					if (rs.next()) {
+						JOptionPane.showMessageDialog(loginP, "@@로그인성공@@");
+		               System.out.println("로그인성공");
+		            } else {
+		            	JOptionPane.showMessageDialog(loginP, "로그인실패!!!");
+		                System.out.println("로그인 실패");
+		            }
+
+		            rs.close();
+		            stmt.close();
+					
+				} catch (SQLException  e2) {
+					e2.printStackTrace();
+				}
+				
+			}
+		});
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
