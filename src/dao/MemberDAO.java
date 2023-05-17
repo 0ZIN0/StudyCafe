3e89f71abf1050ad4db998936be05bdc97990b9d
Gi7A-09 <Gi7A-09@DESKTOP-OI19DI3>
2023-05-17 16:28:34 +0900

    임시

--- src/dao/MemberDAO.java
+++ src/dao/MemberDAO.java
@@ -11,35 +11,5 @@ import javax.swing.JOptionPane;
 import panel.LoginMainPanel;
 
 public class MemberDAO {
-	void checkLogin() {
-		String phon = userPhonNumber.getText();
-		String pass = new String(userPassField.getPassword());
-		
-		
-		try {
-			Connection conn = DriverManager.getConnection(url, user, password);
-			PreparedStatement stmt = conn.prepareStatement("SELECT * FROM member WHERE phone_number=? AND member_password=?");
-            stmt.setString(1, phon);
-            stmt.setString(2, pass);
-            ResultSet rs = stmt.executeQuery();	
-            
-			if (rs.next()) {
-				JOptionPane.showMessageDialog(new LoginMainPanel().background, "@@로그인성공@@");
-               System.out.println("로그인성공");
-               
-            } else {
-            	JOptionPane.showMessageDialog(new LoginMainPanel().background, "로그인실패!!!");
-                System.out.println("로그인 실패");
-            }
-
-            rs.close();
-            stmt.close();
-			
-		} catch (SQLException  e2) {
-			e2.printStackTrace();
-		}
-	}
-	
-	
 	
 }
