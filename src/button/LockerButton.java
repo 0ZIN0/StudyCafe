339669629bc8d1044c9127f4bdae9c04825a7616
Gi7A-09 <Gi7A-09@DESKTOP-OI19DI3>
2023-05-18 13:51:52 +0900

    사물함 기능 완성

--- src/button/LockerButton.java
+++ src/button/LockerButton.java
@@ -12,34 +12,33 @@ import dialog.LockerPayDialog;
 
 public class LockerButton extends JButton implements ActionListener{
 	
-	boolean inUse;
+	int usingLocker;
 	
 	public LockerButton(int lockerNum) {
 		
-		inUse = LockerDAO.inUse(lockerNum);
+		usingLocker = LockerDAO.isUse().indexOf(lockerNum);
 		setText(Integer.toString(lockerNum));
 		setSize(100, 100);
 		setFont(new Font("Noto Sans KR Medium", Font.BOLD, 30));
 		setBorderPainted(false);
 		addActionListener(this);
 		
-		if(inUse) {
+		if(usingLocker >= 0) {
 			setBackground(new Color(0x8D8787));
+			setEnabled(false);
 		} else {
 			setBackground(new Color(217, 217, 217));
+			
 		}
+		System.out.println(usingLocker);
 		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
-		if(!inUse) {
-			LockerPayDialog lockerPayDialog = new LockerPayDialog(getLockerNum());
-		}
-		
+		LockerPayDialog lockerPayDialog = new LockerPayDialog(getLockerNum());
 	}
-	
 	public String getLockerNum() {
 		return getText();
 	}
