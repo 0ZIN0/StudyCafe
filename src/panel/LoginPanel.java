f19db768ec8cfea23e126f83c678d6a93853c5c9
Gi7A-09 <Gi7A-09@DESKTOP-OI19DI3>
2023-05-23 15:50:31 +0900

    최종수정

--- src/panel/LoginPanel.java
+++ src/panel/LoginPanel.java
@@ -95,7 +95,7 @@ public class LoginPanel extends JPanel {
 		
 		// 핸드폰 번호 입력 TextField 
 		userPhonNumber.setFont(new Font("Noto Sans KR Medium", Font.BOLD, 40));
-		userPhonNumber.setText("일일");
+		userPhonNumber.setText(" 핸드폰번호");
 		userPhonNumber.setOpaque(false);
 		userPhonNumber.setBounds(90, 318, 800, 110);
 		userPhonNumber.setSelectionColor(new Color(0, 0, 0, 0));
