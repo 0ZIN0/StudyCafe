021e63963ceaac56c11e254e3eb7fa0d0e7cb688
Gi7A-09 <Gi7A-09@DESKTOP-OI19DI3>
2023-05-26 10:22:33 +0900

    ㅅㅈㅈ

--- src/panel/MyPagePanel.java
+++ src/panel/MyPagePanel.java
@@ -1,16 +1,20 @@
 package panel;
 
 import java.awt.CardLayout;
-import java.awt.Color;
-import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JToggleButton;
+
+import dto.Member;
+import toggle.UseTicketToggle;
+import toggle.UserInfoToggle;
 
 public class MyPagePanel extends JPanel {
 
@@ -20,94 +24,94 @@ public class MyPagePanel extends JPanel {
     JButton logoutBtn = new JButton("로그아웃");
     JButton mainBtn = new JButton("좌석보기");
     
-    // 마이페이지 배경
+    // 마이페이지  배경 이미지
     ImageIcon myPageBg = new ImageIcon("ui/background/MyPage_BG.jpg"); 
     
-    // 마이페이지 안쪽 
-    ImageIcon myPageContBg = new ImageIcon("ui/MyPage/MyPage_Contents.jpg"); 
+    // 마이페이지 콘텐츠 영역 이미지
+    ImageIcon myPageContBg = new ImageIcon("ui/MyPage/MyPage_Contents_Frame.jpg"); 
     
-    // 회원정보, 이용권미지
-    ImageIcon myPageUserInfoBg = new ImageIcon("ui/MyPage/MyPage_UserInfo_Rigth_Frame.png");
-    ImageIcon myPageUseTicketBg = new ImageIcon("ui/MyPage/MyPage_UseTicket_Rigth_Frame.png");
+    ImageIcon userInfoTogOn = new ImageIcon("ui/MyPage/UserInfo_Button.png");
+
+    ImageIcon useTicketOn = new ImageIcon("ui/MyPage/UseTicket_Button.png");
     
     /* 패널 */
     BackgroundPanel myPagebg = new BackgroundPanel(myPageBg);
     BackgroundPanel mpMainCont = new BackgroundPanel(myPageContBg);
     
-    Container myPageContainer = new JPanel();
-    BackgroundPanel userInfoPanel = new BackgroundPanel(myPageUserInfoBg);
-    BackgroundPanel useTicketPanel = new BackgroundPanel(myPageUseTicketBg);
-	
-	// 회원정보 토글 버튼
-	JToggleButton userInfoToggleBtn = createToggleButton("ui/MyPage/UserInfo_Button_Empty.png",
-			"ui/MyPage/UserInfo_Button.png", 10, 190, 484, 258);
-	
-	// 이용권 토글 버튼
-	JToggleButton useTicketToggleBtn = createToggleButton("ui/MyPage/UseTicket_Button_Empty.png",
-			"ui/MyPage/UseTicket_Button.png", 10, 470, 484, 258);
-	
+    JPanel myPageContainer = new JPanel();
+    
+    UserInfoPanel userInfoPanel = new UserInfoPanel();
+    UseTicketPanel useTicketPanel = new UseTicketPanel();
+    
+    JToggleButton userInfoTogBtn = new UserInfoToggle(userInfoTogOn, myPageCard, userInfoPanel, myPageContainer);
+    JToggleButton useTicketTogBtn = new UseTicketToggle(myPageCard, useTicketPanel, myPageContainer);
+    
 	ButtonGroup myPageTogGroup = new ButtonGroup(); // 토글버튼 그룹 지정
-	
 
+	
+	
     public MyPagePanel() {
     	
-
-    	// 기본 배경 설정
+//    	this.member = member;
+//    	
+//    	phoneNum = new JLabel(member.getPhone_number());
+//    	phoneNum.setBounds(getVisibleRect());
+//    	
+    	
     	add(myPagebg);
     	setLayout(null);
 
         // MyPage 배경 설정
     	myPagebg.add(mpMainCont);
+    	mpMainCont.setLayout(null);
         mpMainCont.setBounds(110, 230, 1700, 760);
         
+        // 카드레이아웃 부모 컨테이너
         mpMainCont.add(myPageContainer);
-        myPageContainer.setBounds(510, 10, 1190, 740);
-        myPageContainer.setBackground(new Color(0, 0, 0, 0));
         myPageContainer.setLayout(myPageCard);
+        myPageContainer.setOpaque(false);
+        myPageContainer.setBounds(510, 10, 1190, 740);
+        
+        userInfoPanel.setLayout(null);
+        userInfoPanel.setBounds(510, 10, 1190, 740);
+        
+        useTicketPanel.setLayout(null);
+        useTicketPanel.setBounds(510, 10, 1190, 740);
         
         myPageContainer.add(userInfoPanel, "UserInfomation");
         myPageContainer.add(useTicketPanel, "UseTicket");
         
-        
-        
         // MyPage 토글 버튼 
-        mpMainCont.add(userInfoToggleBtn);
-        mpMainCont.add(useTicketToggleBtn);
+        mpMainCont.add(userInfoTogBtn);
+        mpMainCont.add(useTicketTogBtn);
+        
+        userInfoTogBtn.addActionListener(new ActionListener() {
+        	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				userInfoTogBtn.setIcon(userInfoTogOn);
+				useTicketTogBtn.setIcon(null);
+			}
+		});
+        
+        useTicketTogBtn.addActionListener(new ActionListener() {
+        	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				useTicketTogBtn.setIcon(useTicketOn);
+				userInfoTogBtn.setIcon(null);
+			}
+		});
+        
         
         // MyPage 토글 그룹  
-        myPageTogGroup.add(userInfoToggleBtn);
-        myPageTogGroup.add(useTicketToggleBtn);
-        
+        myPageTogGroup.add(userInfoTogBtn);
+        myPageTogGroup.add(useTicketTogBtn);
         
         
         
     } // end of constructor 
 
-    private JToggleButton createToggleButton(String iconPathBefore, String iconPathAfter, int x, int y, int width, int height) {
-    	// 버튼 선택 전, 후 이미지 절대 경로
-        ImageIcon iconBefore = new ImageIcon(iconPathBefore);
-        ImageIcon iconAfter = new ImageIcon(iconPathAfter);
-
-        // 버튼 기본 상태 설정
-        JToggleButton toggleButton = new JToggleButton(iconBefore);
-        toggleButton.setContentAreaFilled(false);
-        toggleButton.setBorderPainted(false);
-        toggleButton.setFocusPainted(false);
-        toggleButton.setBounds(x, y, width, height);
-
-        // 버튼 클릭 시 이미지 변경 이벤트
-        toggleButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (toggleButton.isSelected()) {
-                    toggleButton.setIcon(iconAfter);
-                } else {
-                    toggleButton.setIcon(iconBefore);
-                }
-            }
-        });
-        
-
-        return toggleButton;
-    }
-}
+} // end of class
